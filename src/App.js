119370549cdb1eda755ec6b201157b518af8d86c
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-26 09:58:38 +0200

    integration of new components into app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* {<Logo /> 
+      <Logo />
       <ImageLinkForm />
+      {/* { 
+      
       <FaceRecognition />} */}
     </div>
   );
