d31582555b3e1ff4f2d4eaed2b9e65fa5eeb0359
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-26 11:43:16 +0200

    added Rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="whithe f3">
+                {'Fabio, your current rank is...'}
+            </div>
+            <div className="whithe f1">
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
