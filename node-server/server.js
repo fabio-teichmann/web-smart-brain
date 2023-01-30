0955966d1fe79b4e8f0ad7bff288385f653b3c32
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-30 08:42:47 +0200

    all required endpoints in place

--- node-server/server.js
+++ node-server/server.js
@@ -1,10 +1,93 @@
 const express = require('express');
 
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
 const app = express();
 
+// app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('this is working');
+    res.send(database.users);
 })
+
+// SIGN-IN
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+    res.json('sign in');
+})
+
+// REGISTER
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    // create new user
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    });
+    res.json(database.users[database.users.length-1]);
+})
+
+// PROFILE
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach( user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    })
+    if (!found) {
+        res.status(404).json('user not found');
+    }
+})
+
+
+// IMAGE
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach( user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        } 
+    })
+    if (!found) {
+        res.status(404).json('user not found');
+    }
+})
+
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
